e54171ea2ae414abca888a1c63217f430a68ab9b
Jordan Alba Reed <albareedjordan@gmail.com>
2024-10-05 22:51:01 -0600

    Sunspot Level Ups
    New script that can be placed on each sunspot to interact with Sappy.
    There is a public variable allowing each sunspot to boost him to a specific level.

---
+++ Assets/Scripts/SunspotCollisions.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunspotCollisions : MonoBehaviour
+{
+    private bool playerIsColliding = false;
+    private GameObject player;
+    private float timer = 0;
+    public float chargeTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerIsColliding){
+            timer += Time.deltaTime;
+            Debug.Log(timer);
+        }
+        if(timer >= chargeTime){
+            UnlockAbility();
+        }
+        if(!playerIsColliding){
+            timer -= Time.deltaTime;
+        }
+    }
+    void OnTriggerEnter(Collider other){
+        if(other.gameObject.CompareTag("Player")){
+            playerIsColliding = true;
+            player = other.gameObject;
+        }
+    }
+    void OnTriggerExit(Collider other){
+        if(other.gameObject.CompareTag("Player")){
+            playerIsColliding = false;
+            player = null;
+        }
+    }
+    private void UnlockAbility(){
+        PlayerAbilities.abilityLevel = 1;
+    }
+}
